ae2a25ea4c9aa5265311818d5017a625118a0958
Cokorda Raka Angga Jananuraga <raka.moviz@gmail.com>
2021-05-16 01:51:45 -0500

    separate eventdenormalizer

---
+++ cmd/cqrskafka_separated_domain/main.go
@@ -0,0 +1,288 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/pkg/errors"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
+
+	"github.com/Shopify/sarama"
+	"github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+)
+
+const (
+	partitions        = 1
+	maxProcessingTime = 2 * time.Second
+	slackTime         = 5 * time.Second
+)
+
+// BookRoomHandler is a command handler, which handles BookRoom command and emits RoomBooked.
+//
+// In CQRS, one command must be handled by only one handler.
+// When another handler with this command is added to command processor, error will be retuerned.
+type BookRoomHandler struct {
+	eventBus *cqrs.EventBus
+}
+
+func (b BookRoomHandler) HandlerName() string {
+	return "BookRoomHandler"
+}
+
+// NewCommand returns type of command which this handle should handle. It must be a pointer.
+func (b BookRoomHandler) NewCommand() interface{} {
+	return &BookRoom{}
+}
+
+func (b BookRoomHandler) Handle(ctx context.Context, c interface{}) error {
+	// c is always the type returned by `NewCommand`, so casting is always safe
+	cmd := c.(*BookRoom)
+
+	// some random price, in production you probably will calculate in wiser way
+	price := (rand.Int63n(40) + 1) * 10
+
+	log.Printf(
+		"Booked %s for %s from %s to %s",
+		cmd.RoomId,
+		cmd.GuestName,
+		time.Unix(cmd.StartDate.Seconds, int64(cmd.StartDate.Nanos)),
+		time.Unix(cmd.EndDate.Seconds, int64(cmd.EndDate.Nanos)),
+	)
+
+	// RoomBooked will be handled by OrderBeerOnRoomBooked event handler,
+	// in future RoomBooked may be handled by multiple event handler
+	if err := b.eventBus.Publish(ctx, &RoomBooked{
+		ReservationId: watermill.NewUUID(),
+		RoomId:        cmd.RoomId,
+		GuestName:     cmd.GuestName,
+		Price:         price,
+		StartDate:     cmd.StartDate,
+		EndDate:       cmd.EndDate,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// OrderBeerOnRoomBooked is a event handler, which handles RoomBooked event and emits OrderBeer command.
+type OrderBeerOnRoomBooked struct {
+	commandBus *cqrs.CommandBus
+}
+
+func (o OrderBeerOnRoomBooked) HandlerName() string {
+	// this name is passed to EventsSubscriberConstructor and used to generate queue name
+	return "OrderBeerOnRoomBooked"
+}
+
+func (OrderBeerOnRoomBooked) NewEvent() interface{} {
+	return &RoomBooked{}
+}
+
+func (o OrderBeerOnRoomBooked) Handle(ctx context.Context, e interface{}) error {
+	event := e.(*RoomBooked)
+
+	orderBeerCmd := &OrderBeer{
+		RoomId: event.RoomId,
+		Count:  rand.Int63n(10) + 1,
+	}
+
+	return o.commandBus.Send(ctx, orderBeerCmd)
+}
+
+// OrderBeerHandler is a command handler, which handles OrderBeer command and emits BeerOrdered.
+// BeerOrdered is not handled by any event handler, but we may use persistent Pub/Sub to handle it in the future.
+type OrderBeerHandler struct {
+	eventBus *cqrs.EventBus
+}
+
+func (o OrderBeerHandler) HandlerName() string {
+	return "OrderBeerHandler"
+}
+
+func (o OrderBeerHandler) NewCommand() interface{} {
+	return &OrderBeer{}
+}
+
+func (o OrderBeerHandler) Handle(ctx context.Context, c interface{}) error {
+	cmd := c.(*OrderBeer)
+
+	if rand.Int63n(10) == 0 {
+		// sometimes there is no beer left, command will be retried
+		return errors.Errorf("no beer left for room %s, please try later", cmd.RoomId)
+	}
+
+	if err := o.eventBus.Publish(ctx, &BeerOrdered{
+		RoomId: cmd.RoomId,
+		Count:  cmd.Count,
+	}); err != nil {
+		return err
+	}
+
+	log.Printf("%d beers ordered to room %s", cmd.Count, cmd.RoomId)
+	return nil
+}
+
+func main() {
+	logger := watermill.NewStdLogger(false, false)
+	cqrsMarshaler := cqrs.ProtobufMarshaler{}
+
+	commandsSubscriberConfig := kafka.DefaultSaramaSubscriberConfig()
+	commandsSubscriberConfig.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	commandsSubscriber, err := kafka.NewSubscriber(
+		kafka.SubscriberConfig{
+			Brokers:               []string{"localhost:9092"},
+			Unmarshaler:           kafka.DefaultMarshaler{},
+			OverwriteSaramaConfig: configsByProcessingTime(commandsSubscriberConfig),
+			ConsumerGroup:         "commands_subscriber",
+		},
+		watermill.NewStdLogger(false, false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	commandsPublisherConfig := kafka.DefaultSaramaSyncPublisherConfig()
+	commandsPublisher, err := kafka.NewPublisher(
+		kafka.PublisherConfig{
+			Brokers:               []string{"localhost:9092"},
+			Marshaler:             kafka.DefaultMarshaler{},
+			OverwriteSaramaConfig: commandsPublisherConfig,
+		},
+		watermill.NewStdLogger(false, false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	eventsPublisherConfig := kafka.DefaultSaramaSyncPublisherConfig()
+	eventsPublisher, err := kafka.NewPublisher(
+		kafka.PublisherConfig{
+			Brokers:               []string{"localhost:9092"},
+			Marshaler:             kafka.DefaultMarshaler{},
+			OverwriteSaramaConfig: eventsPublisherConfig,
+		},
+		watermill.NewStdLogger(false, false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// CQRS is built on messages router. Detailed documentation: https://watermill.io/docs/messages-router/
+	router, err := message.NewRouter(message.RouterConfig{}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	// Simple middleware which will recover panics from event or command handlers.
+	// More about router middlewares you can find in the documentation:
+	// https://watermill.io/docs/messages-router/#middleware
+	//
+	// List of available middlewares you can find in message/router/middleware.
+	router.AddMiddleware(middleware.Recoverer)
+
+	// cqrs.Facade is facade for Command and Event buses and processors.
+	// You can use facade, or create buses and processors manually (you can inspire with cqrs.NewFacade)
+	cqrsFacade, err := cqrs.NewFacade(cqrs.FacadeConfig{
+		GenerateCommandsTopic: func(commandName string) string {
+			return commandName
+		},
+		CommandHandlers: func(cb *cqrs.CommandBus, eb *cqrs.EventBus) []cqrs.CommandHandler {
+			return []cqrs.CommandHandler{
+				BookRoomHandler{eb},
+				OrderBeerHandler{eb},
+			}
+		},
+		CommandsPublisher: commandsPublisher,
+		CommandsSubscriberConstructor: func(handlerName string) (message.Subscriber, error) {
+			// we can reuse subscriber, because all commands have separated topics
+			return commandsSubscriber, nil
+		},
+		GenerateEventsTopic: func(eventName string) string {
+			return "events"
+		},
+		EventHandlers: func(cb *cqrs.CommandBus, eb *cqrs.EventBus) []cqrs.EventHandler {
+			return []cqrs.EventHandler{
+				OrderBeerOnRoomBooked{cb},
+			}
+		},
+		EventsPublisher: eventsPublisher,
+		EventsSubscriberConstructor: func(handlerName string) (message.Subscriber, error) {
+			eventsSubscriberConfig := kafka.DefaultSaramaSubscriberConfig()
+			eventsSubscriberConfig.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+
+			return kafka.NewSubscriber(
+				kafka.SubscriberConfig{
+					Brokers:               []string{"localhost:9092"},
+					Unmarshaler:           kafka.DefaultMarshaler{},
+					OverwriteSaramaConfig: configsByProcessingTime(eventsSubscriberConfig),
+					ConsumerGroup:         handlerName,
+				},
+				watermill.NewStdLogger(false, false),
+			)
+		},
+		Router:                router,
+		CommandEventMarshaler: cqrsMarshaler,
+		Logger:                logger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// publish BookRoom commands every second to simulate incoming traffic
+	go publishCommands(cqrsFacade.CommandBus())
+
+	// processors are based on router, so they will work when router will start
+	if err := router.Run(context.Background()); err != nil {
+		panic(err)
+	}
+}
+
+func publishCommands(commandBus *cqrs.CommandBus) func() {
+	i := 0
+	for {
+		i++
+
+		startDate, err := ptypes.TimestampProto(time.Now())
+		if err != nil {
+			panic(err)
+		}
+
+		endDate, err := ptypes.TimestampProto(time.Now().Add(time.Hour * 24 * 3))
+		if err != nil {
+			panic(err)
+		}
+
+		bookRoomCmd := &BookRoom{
+			RoomId:    fmt.Sprintf("%d", i),
+			GuestName: "John",
+			StartDate: startDate,
+			EndDate:   endDate,
+		}
+		if err := commandBus.Send(context.Background(), bookRoomCmd); err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func configsByProcessingTime(conf *sarama.Config) *sarama.Config {
+	conf.Consumer.Offsets.Initial = sarama.OffsetOldest
+	conf.ChannelBufferSize = 0
+	conf.Consumer.MaxProcessingTime = maxProcessingTime * partitions
+	conf.Consumer.Group.Rebalance.Timeout = (maxProcessingTime * partitions) + slackTime
+	conf.Net.ReadTimeout = (maxProcessingTime * partitions) + slackTime
+	conf.Net.WriteTimeout = (maxProcessingTime * partitions) + slackTime
+	conf.Net.DialTimeout = (maxProcessingTime * partitions) + slackTime
+
+	return conf
+}
---
+++ cmd/cqrskafka_separated_eventdenormalizer/main.go
@@ -0,0 +1,124 @@
+// Sources for https://watermill.io/docs/getting-started/
+package main
+
+import (
+	"context"
+	fmt "fmt"
+	"sync"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+const (
+	partitions        = 1
+	maxProcessingTime = 2 * time.Second
+	slackTime         = 5 * time.Second
+)
+
+type BookingsFinancialReport struct {
+	handledBookings map[string]struct{}
+	totalCharge     int64
+	lock            sync.Mutex
+}
+
+func NewBookingsFinancialReport() *BookingsFinancialReport {
+	return &BookingsFinancialReport{handledBookings: map[string]struct{}{}}
+}
+
+func (b *BookingsFinancialReport) Handle(ctx context.Context, e interface{}) error {
+	// Handle may be called concurrently, so it need to be thread safe.
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	event := e.(*RoomBooked)
+
+	// When we are using Pub/Sub which doesn't provide exactly-once delivery semantics, we need to deduplicate messages.
+	// GoChannel Pub/Sub provides exactly-once delivery,
+	// but let's make this example ready for other Pub/Sub implementations.
+	if _, ok := b.handledBookings[event.ReservationId]; ok {
+		return nil
+	}
+	b.handledBookings[event.ReservationId] = struct{}{}
+
+	b.totalCharge += event.Price
+
+	fmt.Printf(">>> Already booked rooms for $%d\n", b.totalCharge)
+	return nil
+}
+
+func main() {
+	saramaSubscriberConfig := kafka.DefaultSaramaSubscriberConfig()
+	saramaSubscriberConfig.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+
+	subscriber, err := kafka.NewSubscriber(
+		kafka.SubscriberConfig{
+			Brokers:               []string{"localhost:9092"},
+			Unmarshaler:           kafka.DefaultMarshaler{},
+			OverwriteSaramaConfig: configsByProcessingTime(saramaSubscriberConfig),
+			ConsumerGroup:         "bookings_financial_report",
+		},
+		watermill.NewStdLogger(false, false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	messages, err := subscriber.Subscribe(context.Background(), "events")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	process(ctx, messages)
+}
+
+func processMessage(
+	ctx context.Context, cqrsMarshaler cqrs.CommandEventMarshaler,
+	expectedEventName string, msg *message.Message,
+	bookingsFinancialReport *BookingsFinancialReport,
+) {
+	defer msg.Ack()
+
+	messageEventName := cqrsMarshaler.NameFromMessage(msg)
+	if messageEventName != expectedEventName {
+		return
+	}
+
+	event := &RoomBooked{}
+	cqrsMarshaler.Unmarshal(msg, event)
+
+	bookingsFinancialReport.Handle(ctx, event)
+}
+
+func process(ctx context.Context, messages <-chan *message.Message) {
+	cqrsMarshaler := cqrs.ProtobufMarshaler{}
+	bookingsFinancialReport := NewBookingsFinancialReport()
+
+	expectedEvent := &RoomBooked{}
+	expectedEventName := cqrsMarshaler.Name(expectedEvent)
+
+	for msg := range messages {
+		processMessage(
+			ctx, cqrsMarshaler,
+			expectedEventName, msg,
+			bookingsFinancialReport,
+		)
+	}
+}
+
+func configsByProcessingTime(conf *sarama.Config) *sarama.Config {
+	conf.Consumer.Offsets.Initial = sarama.OffsetOldest
+	conf.ChannelBufferSize = 0
+	conf.Consumer.MaxProcessingTime = maxProcessingTime * partitions
+	conf.Consumer.Group.Rebalance.Timeout = (maxProcessingTime * partitions) + slackTime
+	conf.Net.ReadTimeout = (maxProcessingTime * partitions) + slackTime
+	conf.Net.WriteTimeout = (maxProcessingTime * partitions) + slackTime
+	conf.Net.DialTimeout = (maxProcessingTime * partitions) + slackTime
+
+	return conf
+}
